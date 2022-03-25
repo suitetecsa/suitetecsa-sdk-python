f04c7d283d677ba8dd87fd6f384af77d0b931b51
lesclaz <lesclaz95@gmail.com>
2022-03-25 03:02:05 -0400

    DocStrings Añadidos.

--- libsuitetecsa/api.py
+++ libsuitetecsa/api.py
@@ -28,16 +28,30 @@ class UserPortalClient:
         self.session = None
 
     def init_session(self):
+        """
+        Crea una sesión.
+        :return:
+        """
         self.session = UserPortal.create_session()
         self.session.save()
 
     @property
-    def captcha_as_bytes(self):
+    def captcha_as_bytes(self) -> bytes:
+        """
+        Si la sesión no está creada, la crea y devuelve la imagen captcha
+        en formato de bytes.
+        :return: bytes: imagen captcha
+        """
         if not self.session:
             self.init_session()
         return UserPortal.get_captcha(self.session)
 
     def login(self, captcha_code: str):
+        """
+        Inicia sesión en el portal de usuario.
+        :param captcha_code: Código captcha.
+        :return: instancia de esta clase.
+        """
         if not self.session:
             self.init_session()
 
@@ -55,6 +69,11 @@ class UserPortalClient:
         return self
 
     def recharge(self, recharge_code: str):
+        """
+        Recarga el saldo de la cuenta registrada.
+        :param recharge_code: Código de recarga.
+        :return:
+        """
         UserPortal.recharge(
             self.session,
             recharge_code
@@ -69,6 +88,12 @@ class UserPortalClient:
         self.session.save()
 
     def transfer(self, mount_to_transfer: str, account_to_transfer: str):
+        """
+        Transfiere saldo a otra cuenta nauta.
+        :param mount_to_transfer: Monto a transferir.
+        :param account_to_transfer: Cuenta de destino.
+        :return:
+        """
         UserPortal.transfer(
             self.session,
             mount_to_transfer,
@@ -85,6 +110,11 @@ class UserPortalClient:
         self.session.save()
 
     def change_password(self, new_passwrd: str):
+        """
+        Cambia la contraseña de acceso a internet de la cuenta registrada.
+        :param new_passwrd: Nueva contraseña.
+        :return:
+        """
         UserPortal.change_password(
             self.session,
             self.password,
@@ -92,6 +122,11 @@ class UserPortalClient:
         )
 
     def change_email_password(self, new_passwrd: str):
+        """
+        Cambia la contraseña de la cuenta de correo asociada.
+        :param new_passwrd: Nueva contraseña.
+        :return:
+        """
         UserPortal.change_email_password(
             self.session,
             self.password,
@@ -99,6 +134,13 @@ class UserPortalClient:
         )
 
     def get_lasts(self, action: str = "connections", large: int = 5):
+        """
+        Devuelve las últimas `large` `action` realizadas por la cuenta.
+        :param action: Acciones u operaciones que se requieren.
+        :param large: Cantidad de action que se requieren.
+        :return: Lista de (`Connection | Recharge | Transfer` según el valor de
+        `action`).
+        """
         return UserPortal.get_lasts(
             self.session,
             action,
@@ -106,6 +148,12 @@ class UserPortalClient:
         )
 
     def get_connections(self, year: int, month: int):
+        """
+        Devuelve las conexiones realizadas en el periodo mes-año especificado.
+        :param year: Año en el que buscar.
+        :param month: Mes en el que buscar.
+        :return: Lista de Connection en el periodo solicitado.
+        """
         return UserPortal.get_connections(
             self.session,
             year,
@@ -113,6 +161,12 @@ class UserPortalClient:
         )
 
     def get_recharges(self, year: int, month: int):
+        """
+        Devuelve las recargas realizadas en el periodo mes-año especificado.
+        :param year: Año en el que buscar.
+        :param month: Mes en el que buscar.
+        :return: Lista de Recharge en el periodo solicitado.
+        """
         return UserPortal.get_recharges(
             self.session,
             year,
@@ -120,38 +174,72 @@ class UserPortalClient:
         )
 
     def get_transfers(self, year: int, month: int):
+        """
+        Devuelve las transferencias realizadas en el periodo mes-año
+        especificado.
+        :param year: Año en el que buscar.
+        :param month: Mes en el que buscar.
+        :return: Lista de Transfer en el periodo solicitado.
+        """
         return UserPortal.get_transfers(
             self.session,
             year,
             month
         )
 
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
     @property
     def block_date(self):
+        """
+        :return: Fecha de bloqueo de la cuenta registrada.
+        """
         return self.session.block_date if self.session else None
 
     @property
     def delete_date(self):
+        """
+        :return: Fecha de eliminación de la cuenta registrada.
+        """
         return self.session.delete_date if self.session else None
 
     @property
     def account_type(self):
+        """
+        :return: Tipo de cuenta de la cuenta registrada.
+        """
         return self.session.account_type if self.session else None
 
     @property
     def service_type(self):
+        """
+        :return: Tipo de servicio de la cuenta registrada.
+        """
         return self.session.service_type if self.session else None
 
     @property
     def credit(self):
+        """
+        :return: Saldo disponible de la cuenta registrada.
+        """
         return self.session.credit if self.session else None
 
     @property
     def time(self):
+        """
+        :return: Tiempo disponible de la cuenta registrada.
+        """
         return self.session.time if self.session else None
 
     @property
     def mail_account(self):
+        """
+        :return: Cuenta de correo asociada a la cuenta registrada.
+        """
         return self.session.mail_account if self.session else None
 
 
@@ -162,14 +250,25 @@ class NautaClient(object):
         self.session = None
 
     def init_session(self):
+        """
+        Crea una sesión.
+        :return:
+        """
         self.session = Nauta.create_session()
         self.session.save()
 
     @property
     def is_logged_in(self):
+        """
+        :return: True si hay una sesión abierta.
+        """
         return NautaSession.is_logged_in()
 
     def login(self):
+        """
+        Inicia sesión en internet.
+        :return: Instancia de esta clase.
+        """
         if not self.session:
             self.init_session()
 
@@ -185,6 +284,9 @@ class NautaClient(object):
 
     @property
     def user_credit(self):
+        """
+        :return: Saldo disponible de la cuenta.
+        """
         dispose_session = False
         try:
             if not self.session:
@@ -203,6 +305,9 @@ class NautaClient(object):
 
     @property
     def remaining_time(self):
+        """
+        :return: Tiempo disponible de la cuenta registrada.
+        """
         dispose_session = False
         try:
             if not self.session:
@@ -219,6 +324,10 @@ class NautaClient(object):
                 self.session = None
 
     def logout(self):
+        """
+        Cierra la sesión abierta.
+        :return:
+        """
         try:
             Nauta.logout(
                 session=self.session,
@@ -236,6 +345,10 @@ class NautaClient(object):
             )
 
     def load_last_session(self):
+        """
+        Carga la última sesión.
+        :return:
+        """
         self.session = NautaSession.load()
 
     def __enter__(self):
